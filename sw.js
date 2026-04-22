2bacca253a4c8492b374a01cc04d5ddcb3407bd4
Jakesswan <jaco@hadrongrp.com>
2026-04-22 07:38:11 +0200

    hadron-v14

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_VERSION whenever you ship a change so phones pick it up on next launch.
  */
 
-const CACHE_VERSION = 'hadron-v13';
+const CACHE_VERSION = 'hadron-v14';
 const APP_SHELL = [
   './',
   './index.html',
